a4a00993fe31d4dab50da67db009be2cd6b38fc0
robedpixel <3531281-robedpixel@users.noreply.gitlab.com>
2022-07-01 17:50:50 +0800

    more code for paint tool

--- src/painttool.py
+++ src/painttool.py
@@ -9,6 +9,7 @@ class PaintTool(Tool):
         Tool.__init__()
         self.push_button = None
         self.image = None
+        self.paint_radius = 5
 
     def set_image(self, image: QtGui.QImage):
         self.image = image
@@ -22,3 +23,15 @@ class PaintTool(Tool):
     def draw(self, x_pos: int, y_pos: int, effects: deque):
         deque.append(EffectType.RGB, self, x_pos, y_pos)
         # SET PIXEL AT COORDINATES TO BLACK
+        start_x_pos = x_pos -2
+        start_y_pos = y_pos -2
+        if x_pos-2 <0:
+            start_x_pos = 0
+        if y_pos-2<0:
+            start_y_pos = 0
+        for x in range(start_x_pos,start_x_pos+5):
+            for y in range(start_y_pos,start_y_pos+5):
+                if y>self.image.height():
+                    break
+                else:
+                    #self.image.setPixel()
