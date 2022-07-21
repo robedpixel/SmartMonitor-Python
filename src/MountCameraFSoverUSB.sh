e562b1c5d47d59a87a4618139398b6430dc743ef
robedpixel <3531281-robedpixel@users.noreply.gitlab.com>
2022-07-21 12:33:26 +0800

    add untested script to mount camera filesystem over USB

---
+++ src/MountCameraFSoverUSB.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+: '
+Get directory that script is in
+'
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+SCRIPT_DIR = "${SCRIPT_DIR}/airnefpictures"
+
+gphotofs $SCRIPT_DIR
