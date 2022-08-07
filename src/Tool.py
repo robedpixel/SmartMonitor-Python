c24fe7055f22efe406d411fdb66314a35e79a67a
robedpixel <3531281-robedpixel@users.noreply.gitlab.com>
2022-08-07 14:22:29 +0800

    add rectangle drawing tool

--- src/Tool.py
+++ src/Tool.py
@@ -496,7 +496,6 @@ class CropTool(Tool):
         return EffectType.CROP
 
 
-# TODO: untested
 class LineTool(Tool):
 
     def __init__(self):
@@ -580,6 +579,105 @@ class LineTool(Tool):
                 first = False
             else:
                 painter.drawLine(start_point, effect.pos)
+
+    def get_effect_type(self):
+        return EffectType.RGB
+
+
+# TODO: untested
+class RectTool(Tool):
+
+    def __init__(self):
+        Tool.__init__(self)
+        self.push_button = None
+        self.image = None
+        self.paint_radius = list()
+        self.paint_sizes = dict()
+        self.drawing = False
+        self.startPoint = QtCore.QPoint()
+        self.lastPoint = QtCore.QPoint()
+        self.color = QtGui.QColor(QtCore.Qt.black)
+        self.scale = [float(1)]
+        self.current_effect = None
+        self.image_copy = None
+        self.image_copy_two = None
+
+    def set_image(self, image: [QtGui.QImage]):
+        self.image = image
+
+    def set_color(self, color: list[QtGui.QColor]):
+        self.color = color
+
+    def set_scale(self, scale: list[float]):
+        self.scale = scale
+
+    def set_action_list(self, action_list, action_list_state):
+        self.action_list = action_list
+        self.action_list_state = action_list_state
+
+    def set_button(self, QPushButton):
+        self.push_button = QPushButton
+
+    def set_paint_radius(self, paint_sizes: dict, paint_brush_size: list):
+        self.paint_sizes = paint_sizes
+        self.paint_radius = paint_brush_size
+
+    def on_deselect_tool(self):
+        self.push_button.setChecked(False)
+
+    def on_click(self, pos: QtCore.QPoint, effects: deque):
+        self.drawing = True
+        self.image_copy = QtGui.QImage(self.image[0])
+        new_pos = QtCore.QPoint(int(pos.x() / self.scale[0]), int(pos.y() / self.scale[0]))
+        self.startPoint = new_pos
+        self.current_effect = []
+        self.current_effect.append(Effect(new_pos))
+
+    def on_drag(self, pos: QtCore.QPoint, effects: deque):
+        if self.drawing:
+            self.image_copy_two = QtGui.QImage(self.image_copy)
+            painter = QtGui.QPainter(self.image_copy_two)
+            painter.setPen(QtGui.QPen(self.color[0], int(self.paint_sizes[self.paint_radius[0]]),
+                                      QtCore.Qt.SolidLine, QtCore.Qt.RoundCap, QtCore.Qt.RoundJoin))
+            new_pos = QtCore.QPoint(int(pos.x() / self.scale[0]), int(pos.y() / self.scale[0]))
+            pos1 = QtCore.QPoint(self.startPoint.x(), new_pos.y())  # Bottom left point
+            pos2 = QtCore.QPoint(new_pos.x(), self.startPoint.y())  # Top right point
+            painter.drawLine(self.startPoint, pos2)  # top line
+            painter.drawLine(self.startPoint, pos1)  # left line
+            painter.drawLine(pos2, new_pos)  # right line
+            painter.drawLine(pos1, new_pos)  # bottom line
+            self.image[0] = self.image_copy_two
+
+    def on_release(self, pos: QtCore.QPoint, effects: deque):
+        self.drawing = False
+
+        new_pos = QtCore.QPoint(int(pos.x() / self.scale[0]), int(pos.y() / self.scale[0]))
+        self.current_effect.append(Effect(new_pos))
+
+        stop_index = len(self.action_list) - self.action_list_state[0]
+        self.action_list.insert(stop_index, PaintAction(self, self.current_effect, EffectType.RGB,
+                                                        int(self.paint_sizes[self.paint_radius[0]]), self.color[0]))
+        while len(self.action_list) > stop_index + 1:
+            self.action_list.pop()
+        self.action_list_state[0] = 0
+
+    def apply_effect(self, action, image: [QtGui.QImage]):
+        first = True
+        painter = QtGui.QPainter(image[0])
+        start_point = QtCore.QPoint()
+        painter.setPen(QtGui.QPen(action.color, action.radius,
+                                  QtCore.Qt.SolidLine, QtCore.Qt.RoundCap, QtCore.Qt.RoundJoin))
+        for effect in reversed(action.effects):
+            if first:
+                start_point = effect.pos
+                first = False
+            else:
+                pos1 = QtCore.QPoint(start_point.x(), effect.pos.y())  # Bottom left point
+                pos2 = QtCore.QPoint(effect.pos.x(), start_point.y())  # Top right point
+                painter.drawLine(start_point, pos2)  # top line
+                painter.drawLine(start_point, pos1)  # left line
+                painter.drawLine(pos2, effect.pos)  # right line
+                painter.drawLine(pos1, effect.pos)  # bottom line
 
     def get_effect_type(self):
         return EffectType.RGB
