221e7da48ccbccb7c7cfdd225716a5f2bf84044a
robedpixel <3531281-robedpixel@users.noreply.gitlab.com>
2022-09-12 14:14:37 +0800

    renamed class to be more consistent

--- src/DragDropLabel.py
+++ src/DragDropLabel.py
@@ -1,13 +1,8 @@
 from PySide2 import QtWidgets, QtCore, QtGui
 
 
+
 class DragDropImageLabel(QtWidgets.QLabel):
-
-    def __init__(self, press_callback_function, move_callback_function, release_callback_function):
-        QtWidgets.QLabel.__init__(self)
-
-
-class DragDropTextLabel(QtWidgets.QLabel):
 
     def __init__(self):
         QtWidgets.QLabel.__init__(self)
