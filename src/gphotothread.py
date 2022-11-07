6665f46579b261259543cf4e08018be57172cba0
robedpixel <3531281-robedpixel@users.noreply.gitlab.com>
2022-11-07 15:44:40 +0800

    optimisations

--- src/gphotothread.py
+++ src/gphotothread.py
@@ -5,11 +5,14 @@ import platform
 import subprocess
 from glob import glob
 
+
 class GPhotoThread(threading.Thread):
     def __init__(self):
+        self.cF = 0
         self.cFH = 0
         self.copy_point = ""
         self.stopped = False
+        self.connected_camera = None
 
     def connect_to_camera(self, mount_point, copy_point):
         self.copy_point = copy_point
@@ -17,30 +20,34 @@ class GPhotoThread(threading.Thread):
             import gphoto2 as gp
             cameras = gp.gp_camera_autodetect()
             if cameras[0] > 0:
-                p = subprocess.run(["gphotofs", os.path.abspath(mount_point)])
-                if p.returncode == 0:
-                    print("camera filesystem mounted!")
-                    self.cFH = self.countFiles()
-                    return True
+                self.connected_camera = gp.Camera()
+                self.connected_camera.init()
+                #p = subprocess.run(["gphotofs", os.path.abspath(mount_point)])
+                #if p.returncode == 0:
+                print("camera filesystem mounted!")
+                self.cFH = self.count_files_optimised(self.connected_camera)
+                return True
         return False
 
     def run(self):
         while not self.stopped:
-            self.cF = self.countFiles()
+            self.cF = self.count_files_optimised(self.connected_camera)
             if self.cF != self.cFH:
-                #TODO: try to convert to using gphoto2 --get-files if possible
+                # TODO: try to convert to using gphoto2 --get-files if possible
                 print("New files have been taken")
-                uM = subprocess.Popen(['fusermount', "-u", os.path.abspath(mount_point)])
-                m = subprocess.Popen(['gphotofs', os.path.abspath(mount_point)])
-                result = [y for x in os.walk(os.path.abspath(mount_point)) for y in glob(os.path.join(x[0], '*.JPG'))]
+                #uM = subprocess.Popen(['fusermount', "-u", os.path.abspath(mount_point)])
+                #m = subprocess.Popen(['gphotofs', os.path.abspath(mount_point)])
+                #result = [y for x in os.walk(os.path.abspath(mount_point)) for y in glob(os.path.join(x[0], '*.JPG'))]
+                raw_result = self.list_files(self.connected_camera)
+                result = [x for x in raw_result if x.endswith(".JPG")]
                 latest_file = max(result, key=os.path.getctime)
-                shutil.copy(latest_file, self.copy_point+"/"+latest_file.name())
+                shutil.copy(latest_file, self.copy_point + "/" + latest_file.name())
                 self.cFH = self.cF
         print("unmounting camera...")
-        p = subprocess.run(["fusermount", "-u", os.path.abspath(mount_point)])
-        if p.returncode == 0:
-            print("camera filesystem unmounted!")
-            self.camera_mounted = False
+        self.connected_camera.exit()
+        #p = subprocess.run(["fusermount", "-u", os.path.abspath(mount_point)])
+        #if p.returncode == 0:
+        print("camera filesystem unmounted!")
 
     def stop(self):
         self.stopped = True
@@ -55,7 +62,6 @@ class GPhotoThread(threading.Thread):
                 cExist += 1
                 return cExist
 
-
     def countFiles(self):
         fcount = 0
         files = subprocess.Popen(['gphoto2', '-L'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
@@ -66,3 +72,20 @@ class GPhotoThread(threading.Thread):
                 fcount += 1
         print("fcount: " + str(fcount))
         return fcount
+
+    def list_files(self, camera, path='/'):
+        result = []
+        # get files
+        for name, value in camera.folder_list_files(path):
+            result.append(os.path.join(path, name))
+        # read folders
+        folders = []
+        for name, value in camera.folder_list_folders(path):
+            folders.append(name)
+        # recurse over subfolders
+        for name in folders:
+            result.extend(list_files(camera, os.path.join(path, name)))
+        return result
+
+    def count_files_optimised(self, camera):
+        return len(self.list_files(camera))
