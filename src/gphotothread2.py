2062e5b7e86ee0ad001b94292ae86cbabfde23b2
robedpixel <3531281-robedpixel@users.noreply.gitlab.com>
2023-02-24 14:01:52 +0800

    update gphotothread to upload multiple files

--- src/gphotothread2.py
+++ src/gphotothread2.py
@@ -81,17 +81,20 @@ class GPhotoThread2(threading.Thread):
                     for row in raw_result:
                         if row.endswith(".JPG"):
                             result.append(row)
-                    latest_file = max(result, key=lambda item: self.get_file_time(item))
+
+                    result.sort(key=lambda item: self.get_file_time(item), reverse=True)
+                    numfiles = (self.cF - self.cFH) // 2
                     # get file from latest_file
-                    folder, name = os.path.split(latest_file)
-                    camera_file = gp.check_result(
-                        gp.gp_camera_file_get(self.connected_camera, folder, name.rsplit('.', 1)[0] + ".NEF",
-                                              gp.GP_FILE_TYPE_NORMAL))
-                    gp.check_result(
-                        gp.gp_file_save(camera_file, self.copy_point + "/" + name.rsplit('.', 1)[0] + ".NEF"))
-                    camera_file = gp.check_result(
-                        gp.gp_camera_file_get(self.connected_camera, folder, name, gp.GP_FILE_TYPE_NORMAL))
-                    gp.check_result(gp.gp_file_save(camera_file, self.copy_point + "/" + name))
+                    for i in range(numfiles):
+                        folder, name = os.path.split(result[i])
+                        camera_file = gp.check_result(
+                            gp.gp_camera_file_get(self.connected_camera, folder, name.rsplit('.', 1)[0] + ".NEF",
+                                                  gp.GP_FILE_TYPE_NORMAL))
+                        gp.check_result(
+                            gp.gp_file_save(camera_file, self.copy_point + "/" + name.rsplit('.', 1)[0] + ".NEF"))
+                        camera_file = gp.check_result(
+                            gp.gp_camera_file_get(self.connected_camera, folder, name, gp.GP_FILE_TYPE_NORMAL))
+                        gp.check_result(gp.gp_file_save(camera_file, self.copy_point + "/" + name))
                 self.cFH = self.cF
 
     def shutdown(self):
