69ded28c07c15ab3a738abddd4a02e30534771d6
RobedPixel <18674579+robedpixel@users.noreply.github.com>
2023-03-10 12:38:35 +0800

    add check for folder present when deleting old photos

--- src/gphotothread2.py
+++ src/gphotothread2.py
@@ -18,7 +18,6 @@ class GPhotoThread2(threading.Thread):
         self.connected_camera = None
         self.detector = None
         self.camera_is_connected = False
-        self.running = True
 
     def mount_camera(self):
         cameras = gp.gp_camera_autodetect()
@@ -101,39 +100,38 @@ class GPhotoThread2(threading.Thread):
 
     def run(self):
         while not self.stopped:
-            if self.running:
-                print("getting new pictures...")
-                self.get_updated_camera()
-                if self.camera_is_connected:
-                    self.cF = self.count_files_optimised(self.connected_camera)
-                    if self.cF != self.cFH:
-                        print("New files have been taken")
-                        if self.cF > self.cFH:
-                            raw_result = self.list_files(self.connected_camera)
-                            result = list()
-                            for row in raw_result:
-                                if row.endswith(".JPG"):
-                                    result.append(row)
-                            result.sort(key=lambda item: self.get_file_time(item), reverse=True)
-                            num_files = (self.cF - self.cFH) // 2
-                            # get file from latest_file
-                            for i in range(num_files):
-                                folder, name = os.path.split(result[i])
-                                camera_file = gp.check_result(
-                                    gp.gp_camera_file_get(self.connected_camera, folder,
-                                                          name.rsplit('.', 1)[0] + ".NEF",
-                                                          gp.GP_FILE_TYPE_NORMAL))
-                                gp.check_result(
-                                    gp.gp_file_save(camera_file,
-                                                    self.copy_point + "/" + name.rsplit('.', 1)[0] + ".NEF"))
-                                camera_file = gp.check_result(
-                                    gp.gp_camera_file_get(self.connected_camera, folder, name, gp.GP_FILE_TYPE_NORMAL))
-                                gp.check_result(gp.gp_file_save(camera_file, self.copy_point + "/" + name))
-                        self.cFH = self.cF
-                for i in range(60):
-                    if self.stopped:
-                        break
-                    time.sleep(5)
+            print("getting new pictures...")
+            self.get_updated_camera()
+            if self.camera_is_connected:
+                self.cF = self.count_files_optimised(self.connected_camera)
+                if self.cF != self.cFH:
+                    print("New files have been taken")
+                    if self.cF > self.cFH:
+                        raw_result = self.list_files(self.connected_camera)
+                        result = list()
+                        for row in raw_result:
+                            if row.endswith(".JPG"):
+                                result.append(row)
+                        result.sort(key=lambda item: self.get_file_time(item), reverse=True)
+                        num_files = (self.cF - self.cFH) // 2
+                        # get file from latest_file
+                        for i in range(num_files):
+                            folder, name = os.path.split(result[i])
+                            camera_file = gp.check_result(
+                                gp.gp_camera_file_get(self.connected_camera, folder,
+                                                      name.rsplit('.', 1)[0] + ".NEF",
+                                                      gp.GP_FILE_TYPE_NORMAL))
+                            gp.check_result(
+                                gp.gp_file_save(camera_file,
+                                                self.copy_point + "/" + name.rsplit('.', 1)[0] + ".NEF"))
+                            camera_file = gp.check_result(
+                                gp.gp_camera_file_get(self.connected_camera, folder, name, gp.GP_FILE_TYPE_NORMAL))
+                            gp.check_result(gp.gp_file_save(camera_file, self.copy_point + "/" + name))
+                    self.cFH = self.cF
+            for i in range(60):
+                if self.stopped:
+                    break
+                time.sleep(5)
         if self.camera_is_connected:
             print("unmounting camera...")
             self.connected_camera.exit()
